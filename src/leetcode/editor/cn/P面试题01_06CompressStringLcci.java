a04126efd32e4d84febbe36d676f6d60ca3238cd
livinglh <390866592@qq.com>
2020-03-16 00:34:33 +0800

    添加面试题01_06

---
+++ src/leetcode/editor/cn/P面试题01_06CompressStringLcci.java
@@ -0,0 +1,63 @@
+//字符串压缩。利用字符重复出现的次数，编写一种方法，实现基本的字符串压缩功能。比如，字符串aabcccccaaa会变为a2b1c5a3。若“压缩”后的字符串没
+//有变短，则返回原先的字符串。你可以假设字符串中只包含大小写英文字母（a至z）。 
+//
+// 示例1: 
+//
+// 
+// 输入："aabcccccaaa"
+// 输出："a2b1c5a3"
+// 
+//
+// 示例2: 
+//
+// 
+// 输入："abbccd"
+// 输出："abbccd"
+// 解释："abbccd"压缩后为"a1b2c2d1"，比原字符串长度更长。
+// 
+//
+// 提示： 
+//
+// 
+// 字符串长度在[0, 50000]范围内。 
+// 
+// Related Topics 字符串
+
+  
+package leetcode.editor.cn;
+//java:字符串压缩
+public class P面试题01_06CompressStringLcci{
+    public static void main(String[] args) {
+        Solution solution = new P面试题01_06CompressStringLcci().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String compressString(String S) {
+        if (S == null || S.length() == 0 || S.length() == 1) return S;
+        String s = "";
+        char pre = '#';
+        int sum = 0;
+        for (int i = 0; i < S.length(); i++) {
+            if(pre != S.charAt(i)){
+                s = s + String.valueOf(pre);
+                s = s + String.valueOf(sum);
+                pre = S.charAt(i);
+                sum = 1;
+                if(i == S.length() - 1){
+                    s = s + String.valueOf(pre);
+                    s = s + String.valueOf(sum);
+                }
+            } else if(i == S.length()-1) {
+                s = s + String.valueOf(pre);
+                s = s + String.valueOf(sum+1);
+            } else {
+                sum ++;
+            }
+
+        }
+        return S.length() <= s.length()-2 ? S : s.substring(2,s.length());
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
