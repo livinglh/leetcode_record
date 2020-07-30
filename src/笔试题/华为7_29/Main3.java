9f7d412e927dbd8d9934d01a09b9f9215c1abe77
livinglh <390866592@qq.com>
2020-07-30 16:30:01 +0800

    解决第3题结构体循环依赖问题

--- src/笔试题/华为7_29/Main3.java
+++ src/笔试题/华为7_29/Main3.java
@@ -5,7 +5,8 @@ import java.util.*;
 public class Main3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Map<String, List<String>> map = new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>();  // key: 结构题名称 value：该结构体内所含结构体列表
+        Set<String> set = new HashSet<>();  // 记录需要计算的结构题
         String start;
         while(true){
             String s = sc.nextLine();
@@ -33,24 +34,47 @@ public class Main3 {
             }
         }
 
-        int ans = DFS(map, start);
 
+        int ans = DFS(map, set, start);
         System.out.println(ans);
     }
-
-    public static int DFS(Map<String, List<String>> map, String s){
+    // set解决循环依赖问题
+    public static int DFS(Map<String, List<String>> map, Set<String> set, String s){
         if(s.equals("char")) return 1;
         if(s.equals("short")) return 2;
         if(s.equals("long")) return 4;
-        if(!map.containsKey(s)) return 0;
+        if(!map.containsKey(s) || set.contains(s)) return 0;
         List<String> slist = map.get(s);
         int res = 0;
         for(String sl : slist){
-            int r = DFS(map, sl);
-            if(r == -1) return 0;
+            set.add(sl);
+            int r = DFS(map, set, sl);
+            set.remove(sl);
+            if(r == 0) return 0;
             res += r;
         }
         return res;
     }
 }
 
+/*
+struct s1 {
+    short a;
+    long b;
+    char c;
+    struct s2 d;
+};
+struct s2 {
+    short a;
+    long b;
+    char c;
+    struct s3 d;
+};
+struct s3 {
+    short a;
+    long b;
+    char c;
+    struct s1 d;
+};
+sizeof(s3);
+ */
