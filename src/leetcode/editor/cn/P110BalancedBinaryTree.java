ed660acb4c6c138377399af579df9ce6ae20886e
livinglh <390866592@qq.com>
2020-08-17 12:14:51 +0800

    添加110

---
+++ src/leetcode/editor/cn/P110BalancedBinaryTree.java
@@ -0,0 +1,88 @@
+//给定一个二叉树，判断它是否是高度平衡的二叉树。 
+//
+// 本题中，一棵高度平衡二叉树定义为： 
+//
+// 
+// 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。 
+// 
+//
+// 示例 1: 
+//
+// 给定二叉树 [3,9,20,null,null,15,7] 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7 
+//
+// 返回 true 。 
+// 
+//示例 2: 
+//
+// 给定二叉树 [1,2,2,3,3,null,null,4,4] 
+//
+//        1
+//      / \
+//     2   2
+//    / \
+//   3   3
+//  / \
+// 4   4
+// 
+//
+// 返回 false 。 
+//
+// 
+// Related Topics 树 深度优先搜索 
+// 👍 418 👎 0
+
+
+package leetcode.editor.cn;
+
+//java:平衡二叉树
+public class P110BalancedBinaryTree {
+    public static void main(String[] args) {
+        Solution solution = new P110BalancedBinaryTree().new Solution();
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public boolean isBalanced(TreeNode root) {
+            if(root == null) {
+                return true;
+            }else{
+                return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+            }
+        }
+
+        public int height(TreeNode root){
+            if(root == null){
+                return 0;
+            }
+            return Math.max(height(root.left), height(root.right)) + 1;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
